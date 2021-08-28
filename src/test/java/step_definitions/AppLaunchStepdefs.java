4bdd0ed47c084812229653bc4c1e3f083a4abbc5
PtrHtn <ptrhtn@gmail.com>
2021-08-28 18:50:51 -0300

    Generate step definitions for 'app launch scenarios'

---
+++ src/test/java/step_definitions/AppLaunchStepdefs.java
@@ -0,0 +1,14 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AppLaunchStepdefs {
+    @Given("an app is at the Baby Dots home screen")
+    public void anAppIsAtTheBabyDotsHomeScreen() {
+    }
+
+    @Then("title {string} is shown on the screen")
+    public void titleIsShownOnTheScreen(String arg0) {
+    }
+}
