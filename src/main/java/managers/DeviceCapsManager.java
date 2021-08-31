00c40f290223bb1cc21808800bb4620e9c8405e0
PtrHtn <ptrhtn@gmail.com>
2021-08-31 20:18:48 -0300

    Add device capabilities manager

---
+++ src/main/java/managers/DeviceCapsManager.java
@@ -0,0 +1,13 @@
+package managers;
+
+public class DeviceCapsManager {
+    private String udid;
+
+    public String getUDID() {
+        return udid;
+    }
+
+    public void setUDID(String newUDID) {
+        udid = newUDID;
+    }
+}
