8deef5fc1dab6614e50db682fef404ca20a87580
PtrHtn <ptrhtn@gmail.com>
2021-09-06 19:08:42 -0300

    Add waitForVisibilityOf(MobileElement element) method

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,24 @@
 package pages;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import managers.DriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
     private final AndroidDriver<?> driver;
 
     public BasePage() {
         this.driver = new DriverManager().getDriver();
+    }
+
+    private void waitForVisibilityOf(MobileElement element) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, WAIT_TIME);
+            wait.until(ExpectedConditions.visibilityOf(element));
+        }catch (Exception exception) {
+            System.out.println("Something went wrong in waitForVisibilityOf method: " + exception);
+        }
     }
 }
