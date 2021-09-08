a7553a43723b84fabcfed8233c33608189f4dc84
PtrHtn <ptrhtn@gmail.com>
2021-09-08 00:52:03 -0300

    Add toolbarLocator into AppBar component

--- src/test/java/pages/components/AppBar.java
+++ src/test/java/pages/components/AppBar.java
@@ -1,6 +1,11 @@
 package pages.components;
 
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
 import pages.BasePage;
 
 public class AppBar extends BasePage {
+
+    private final By toolbarLocator = MobileBy.id("toolbar");
+
 }
