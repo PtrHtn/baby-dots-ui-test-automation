7b9d3a6e4f592622f4c7b103aeb24aed5c1dcbf2
PtrHtn <ptrhtn@gmail.com>
2021-09-09 20:49:25 -0300

    Add assertation into anAppIsAtTheBabyDotsHomeScreen

--- src/test/java/step_definitions/AppLaunchStepdefs.java
+++ src/test/java/step_definitions/AppLaunchStepdefs.java
@@ -2,13 +2,16 @@ package step_definitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.testng.Assert;
+import pages.HomePage;
 
 public class AppLaunchStepdefs {
     @Given("an app is at the Baby Dots home screen")
     public void anAppIsAtTheBabyDotsHomeScreen() {
+        Assert.assertTrue(new HomePage().appBar().titleIsDisplayed());
     }
 
-    @Then("title {string} is shown on the screen")
-    public void titleIsShownOnTheScreen(String arg0) {
+    @Then("title {title} is shown on the screen")
+    public void titleIsShownOnTheScreen(String title) {
     }
 }
