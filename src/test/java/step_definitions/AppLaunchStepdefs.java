5b47848e301aeb2c0f520edbe93bb1c616b67652
PtrHtn <ptrhtn@gmail.com>
2021-09-18 20:18:23 -0300

    Add lockIconIsDisplayed assertation

--- src/test/java/step_definitions/AppLaunchStepdefs.java
+++ src/test/java/step_definitions/AppLaunchStepdefs.java
@@ -1,19 +1,20 @@
 package step_definitions;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.testng.Assert;
 import pages.HomePage;
 
 public class AppLaunchStepdefs {
-    @Given("an app is at the Baby Dots home screen")
+    @When("an app is at the Baby Dots home screen")
     public void anAppIsAtTheBabyDotsHomeScreen() {
         Assert.assertTrue(new HomePage().appBar().titleIsDisplayed());
         Assert.assertTrue(new HomePage().appBar().soundIconIsDisplayed());
+        Assert.assertTrue(new HomePage().appBar().lockIconIsDisplayed());
 
     }
 
-    @Then("title {title} is shown on the screen")
+    @Then("title {string} is shown on the screen")
     public void titleIsShownOnTheScreen(String title) {
         Assert.assertEquals(new HomePage().appBar().getTitle(), title);
     }
